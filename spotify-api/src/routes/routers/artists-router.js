717234ddaa1df6b3c2227fc4d1744da21da442a0
ALOnelyAVacado <Stefan.sutherland1@outlook.com>
2024-05-26 17:34:36 +0200

    Added search  related artists, and search artist tracks.

--- spotify-api/src/routes/routers/artists-router.js
+++ spotify-api/src/routes/routers/artists-router.js
@@ -1,8 +1,10 @@
-const { searchArtists, searchArtist, searchArtistAlbums } = require('../../controllers/artists-controller');
+const { searchArtists, searchArtist, searchArtistAlbums, searchArtistTracks, searchRelatedArtist } = require('../../controllers/artists-controller');
 const artistsRouter = require('express').Router();
 
 artistsRouter.get('', searchArtists);
 artistsRouter.get('/artist', searchArtist);
-artistsRouter.get('/artistAlbums', searchArtistAlbums);
+artistsRouter.get('/albums', searchArtistAlbums);
+artistsRouter.get('/tracks', searchArtistTracks);
+artistsRouter.get('/related', searchRelatedArtist);
 
 module.exports = artistsRouter;
