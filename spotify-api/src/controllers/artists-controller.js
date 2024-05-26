ea535f154345f5b8d711c62b039cb2ddc5d52433
SteveNugget2 <65278710+SteveNugget2@users.noreply.github.com>
2024-05-26 17:49:49 +0200

    Spotify (#7)
    * Added search artist, search 50 artists and get artist albums
    
    * Added search  related artists, and search artist tracks.

--- spotify-api/src/controllers/artists-controller.js
+++ spotify-api/src/controllers/artists-controller.js
@@ -2,7 +2,7 @@ const spotifyApi = require('../lib/spotify-api');
 const {Request, Response} = require('express');
 
 /**
- * Route returning all artists related to a query parameter.
+ * Route returning all ()artists related to a query parameter.
  *
  * @param {Request} req - The request object.
  * @param {Response} res - The response object.
@@ -11,7 +11,7 @@ const {Request, Response} = require('express');
 const searchArtists = async (req, res) => {
     try {
         const { name } = req.query;
-        const artists = await spotifyApi.search(name , ["artist"]);
+        const artists = await spotifyApi.search(name , ["artist"], 'ZA', 50);
         res.status(200).json({
             data: artists
         });
@@ -23,6 +23,70 @@ const searchArtists = async (req, res) => {
     }
 }
 
+const searchArtist = async (req, res) => {
+    try {
+        const { artistID } = req.query;
+        const artist = await spotifyApi.artists.get(artistID);
+        res.status(200).json({
+            data: artist
+        });
+    } catch(e) {
+        // replace with custom status code if needed
+        res.status(500).json({
+            message: e.message
+        })
+    }
+}
+
+const searchArtistAlbums = async (req, res) => {
+    try {
+        const { artistID } = req.query;
+        const artistAlbums = await spotifyApi.artists.albums(artistID);
+        res.status(200).json({
+            data: artistAlbums
+        });
+    } catch(e) {
+        // replace with custom status code if needed
+        res.status(500).json({
+            message: e.message
+        })
+    }
+}
+
+const searchArtistTracks = async (req, res) => {
+    try {
+        const { artistID } = req.query;
+        const artistTracks = await spotifyApi.artists.topTracks(artistID);
+        res.status(200).json({
+            data: artistTracks
+        });
+    } catch(e) {
+        // replace with custom status code if needed
+        res.status(500).json({
+            message: e.message
+        })
+    }
+}
+
+const searchRelatedArtist = async (req, res) => {
+    try {
+        const { artistID } = req.query;
+        const relatedArtists = await spotifyApi.artists.relatedArtists(artistID);
+        res.status(200).json({
+            data: relatedArtists
+        });
+    } catch(e) {
+        // replace with custom status code if needed
+        res.status(500).json({
+            message: e.message
+        })
+    }
+}
+
 module.exports = {
     searchArtists,
+    searchArtist,
+    searchArtistAlbums,
+    searchArtistTracks,
+    searchRelatedArtist,
 }
--- spotify-api/src/routes/routers/artists-router.js
+++ spotify-api/src/routes/routers/artists-router.js
@@ -1,6 +1,10 @@
-const { searchArtists } = require('../../controllers/artists-controller');
+const { searchArtists, searchArtist, searchArtistAlbums, searchArtistTracks, searchRelatedArtist } = require('../../controllers/artists-controller');
 const artistsRouter = require('express').Router();
 
 artistsRouter.get('', searchArtists);
+artistsRouter.get('/artist', searchArtist);
+artistsRouter.get('/albums', searchArtistAlbums);
+artistsRouter.get('/tracks', searchArtistTracks);
+artistsRouter.get('/related', searchRelatedArtist);
 
 module.exports = artistsRouter;
