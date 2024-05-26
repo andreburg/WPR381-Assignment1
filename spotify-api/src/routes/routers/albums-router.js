65ce4f3093e71a0aa4c06ff798c30e0df0c267cb
AdrianConradie <adriancconradie@gmail.com>
2024-05-26 17:49:30 +0200

    Adrian (#6)
    * LoadArtistAlbums
    
    * LoadArtistAlbums2

--- spotify-api/src/routes/routers/albums-router.js
+++ spotify-api/src/routes/routers/albums-router.js
@@ -1,6 +1,6 @@
-const { getArtistAlbums } = require('../../controllers/albums-controller');
-const albumsRouter = require('express').Router();
+const { getArtistAlbums } = require("../../controllers/albums-controller");
+const albumsRouter = require("express").Router();
 
-albumsRouter.get('', getArtistAlbums)
+albumsRouter.get("", getArtistAlbums);
 
 module.exports = albumsRouter;
