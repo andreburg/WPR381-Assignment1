9c509a3163c821a3fc76cd35838c7930d87fc6dc
andre <andreludovicusburger@gmail.com>
2024-05-26 17:51:42 +0200

    move api object to spotify lib

--- spotify-api/src/lib/spotify-api.js
+++
@@ -1,8 +0,0 @@
-const { SpotifyApi } = require("@spotify/web-api-ts-sdk");
-
-const spotifyApi = SpotifyApi.withClientCredentials(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET
-);
-
-module.exports = spotifyApi;
