a756497a77aae2a4c680bd953b833314e2a405b4
AdrianConradie <adriancconradie@gmail.com>
2024-05-26 17:14:15 +0200

    LoadArtistAlbums

--- spotify-api/src/controllers/albums-controller.js
+++ spotify-api/src/controllers/albums-controller.js
@@ -1,56 +1,55 @@
-const spotifyApi = require('../lib/spotify-api');
-const {Request, Response} = require('express');
+const spotifyApi = require("../lib/spotify-api");
+const { Request, Response } = require("express");
 
 /**
  * Route returning all songs related to an artist.
  *
  * @param {Request} req - The request object.
  * @param {Response} res - The response object.
- * 
+ *
  */
 const getArtistAlbums = async (req, res) => {
-    try {
-        const albums = [];
-
-        //replace with logic
-
-        res.status(200).json({
-            data: albums
-        })
-    } catch(e) {
-        // replace with custom status code if needed
-        res.status(500).json({
-            message: e.message
-        })
-    }
-}
+  try {
+    //const albums = [];
+    //const { id } = req.query;
+    //temp ="4llAOeA6kEF4ytaB2fsmcW";
+    albums = await spotifyApi.artists.albums(req.query.artist_id);
+    res.status(200).json({
+      data: albums,
+    });
+  } catch (e) {
+    // replace with custom status code if needed
+    res.status(500).json({
+      message: e.message,
+    });
+  }
+};
 
 /**
  * Route returning top 100 albums, with optional genre filter `/:genre`.
  *
  * @param {Request} req - The request object.
  * @param {Response} res - The response object.
- * 
+ *
  */
 const getTop100Albums = async (req, res) => {
-    try {
-        const albums = [];
+  try {
+    const albums = [];
 
-        //replace with logic
+    //replace with logic
 
-        res.status(200).json({
-            data: albums
-        })
-    } catch(/** @type {Error} */e) {
-        // replace with custom status code if needed
-        res.status(500).json({
-            message: e.message
-        })
-    }
-}
-
+    res.status(200).json({
+      data: albums,
+    });
+  } catch (/** @type {Error} */ e) {
+    // replace with custom status code if needed
+    res.status(500).json({
+      message: e.message,
+    });
+  }
+};
 
 module.exports = {
-    getArtistAlbums,
-    getTop100Albums,
-}
+  getArtistAlbums,
+  getTop100Albums,
+};
